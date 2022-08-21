30141a33222995b39165e75409ecf3ae68b0c6b3
Zane Sharif <Zane.Sharif@gmail.com>
2022-08-20 19:50:17 -0700

    starting routes

---
+++ utils/data.js
@@ -0,0 +1,45 @@
+const users = [
+    {
+        username: 'Varthara',
+        email: 'varthara@gmail.com',
+    },
+    {
+        username: 'Jimbo',
+        email: 'Jimbo@gmail.com',
+    },
+    {
+        username: 'Willy',
+        email: 'Willy@gmail.com',
+    },
+    {
+        username: 'Bobo',
+        email: 'Bobo@gmail.com',
+    },
+]
+
+const thoughts = [
+    {
+        thoughtText: 'woogieboogie',
+        username: 'Varthara',
+    },
+    {
+        thoughtText: 'howdyhowdy',
+        username: 'Bobo',
+    },
+    {
+        thoughtText: 'whowho',
+        username: 'Jimbo',
+    },
+    {
+        thoughtText: 'meme?',
+        username: 'Willy',
+    },
+    {
+        thoughtText: 'whenwhen',
+        username: 'Jimbo',
+    },
+    {
+        thoughtText: 'testest',
+        username: 'Varthara',
+    },
+]
---
+++ utils/seed.js
@@ -0,0 +1,19 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const {
+    users,
+    thoughts
+} = require('./data');
+
+connection.once('open', async () => {
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+
+
+    await User.collection.insertMany(users);
+    await Thought.collection.insertMany(thoughts);
+
+    console.table(users);
+    console.table(thoughts);
+    process.exit(0);
+})
